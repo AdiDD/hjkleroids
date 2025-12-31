a1a14d17ccd134cda684a64765a2567059badfdb
Adi Deaconu <adi.deaconu21@gmail.com>
2025-12-31 13:00:33 +0200

    draw player

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH  =  1280  
+SCREEN_HEIGHT = 720   
+PLAYER_RADIUS = 20    # The radius of the ship
+LINE_WIDTH    = 2     # The width of the lines that draw the ship 
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
-    pygame.init()
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Starting Asteroids with pygame verson {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init() 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    clock = pygame.time.Clock()
+    dt = 0.0 # delta time in seconds
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,11 +24,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
-    print(f"Starting Asteroids with pygame verson {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        # Cap at 60 fps
+        dt = clock.tick(60) / 1_000
 
 
 if __name__ == "__main__":
